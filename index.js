c5bdb9c620bde7d301c5065ae706a5cd825a74f0
Manan21st <manan9346@gmail.com>
2024-07-01 11:02:16 +0530

    file system

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const fs = require('fs');
 
 // Read from the file
-fs.readFile('file.txt', 'utf8', (err, data) => {
-    console.log('File content:', data);
+// fs.readFileSync('file.txt', 'utf8', (err, data) => {
+//     console.log('File content:', data);
+// });
+// fs.writeFileSync('file.txt', 'OverWritten content', 'utf8', () => {
+//     console.log('File has been written.');
+// });
+// fs.appendFileSync('file.txt', " class is going on", 'utf8', ()=> {
+//     console.log('File has been appended.');
+// });
 
-    const newContent = 'OverWritten content';
-    fs.writeFile('file.txt', newContent, 'utf8', () => {
-        console.log('File has been written.');
-    });
-});
+// fs.mkdir('directory1',(err) => {
+//     if(err) {
+//         console.log('Error:', err);
+//     } else {
+//         console.log('Directory created successfully.');
+//     }
+// });
+
+//fs.rmdir('directory1', (err) => {
+//     if(err) {
+//         console.log('Error:', err);
+//     } else {
+//         console.log('Directory deleted successfully.');
+//     }
+// });
+
+// fs.rename('new-file.txt', 'file.txt', (err) => {
+//     if(err) {
+//         console.log('Error:', err);
+//     } else {
+//         console.log('File renamed successfully.');
+//     }
+// });
+
+
+const path = require('path');
+const p = '/Users/Manan Agrawal/Documents/Scaler Codes/Trimester 4/node_practice/file.txt';
+
+const dirname = path.dirname(p);
+const extension = path.extname(p);
+
+console.log('directory name : ',dirname);
+console.log('extension name : ',extension);
